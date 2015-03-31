447c8ff6da9fefeff287b9c432a3e5b28d43d9fd
Katie Tran <katie.m.tran@gmail.com>
2015-03-30 17:36:20 -0700

    added new grad app form
    still WIP

---
+++ GradAppTracker/GradAppTracker/CreateNewGradAppForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace GradAppTracker
+{
+    partial class CreateNewGradAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateNewGradAppForm));
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.tbAdvisorID = new System.Windows.Forms.TextBox();
+            this.tbDeptChairID = new System.Windows.Forms.TextBox();
+            this.tbStudentID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbMajorID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(71, 200);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(126, 33);
+            this.CreateButton.TabIndex = 5;
+            this.CreateButton.Text = "Create";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Location = new System.Drawing.Point(8, 200);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(58, 33);
+            this.CancelButton.TabIndex = 4;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // tbAdvisorID
+            // 
+            this.tbAdvisorID.Location = new System.Drawing.Point(77, 41);
+            this.tbAdvisorID.Name = "tbAdvisorID";
+            this.tbAdvisorID.Size = new System.Drawing.Size(120, 20);
+            this.tbAdvisorID.TabIndex = 2;
+            // 
+            // tbDeptChairID
+            // 
+            this.tbDeptChairID.Location = new System.Drawing.Point(77, 73);
+            this.tbDeptChairID.Name = "tbDeptChairID";
+            this.tbDeptChairID.Size = new System.Drawing.Size(120, 20);
+            this.tbDeptChairID.TabIndex = 3;
+            // 
+            // tbStudentID
+            // 
+            this.tbStudentID.Location = new System.Drawing.Point(77, 9);
+            this.tbStudentID.Name = "tbStudentID";
+            this.tbStudentID.Size = new System.Drawing.Size(120, 20);
+            this.tbStudentID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Advisor ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dept Chair ID";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(8, 234);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(189, 57);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(8, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 2);
+            this.label4.TabIndex = 10;
+            // 
+            // tbMajorID
+            // 
+            this.tbMajorID.Location = new System.Drawing.Point(77, 102);
+            this.tbMajorID.Name = "tbMajorID";
+            this.tbMajorID.Size = new System.Drawing.Size(120, 20);
+            this.tbMajorID.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Major ID";
+            // 
+            // CreateNewGradAppForm
+            // 
+            this.AcceptButton = this.CreateButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelButton;
+            this.ClientSize = new System.Drawing.Size(203, 294);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbMajorID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbStudentID);
+            this.Controls.Add(this.tbDeptChairID);
+            this.Controls.Add(this.tbAdvisorID);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.CreateButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CreateNewGradAppForm";
+            this.Text = "Add New Graduation App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.TextBox tbAdvisorID;
+        private System.Windows.Forms.TextBox tbDeptChairID;
+        private System.Windows.Forms.TextBox tbStudentID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbMajorID;
+        private System.Windows.Forms.Label label5;
+    }
+}
